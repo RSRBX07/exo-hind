fdab939ae002b3bec97755a17d03a0051a29aac5
hind <hind.lepert@cefimis.com>
2017-01-09 14:31:38 +0100

    fonction times

--- hello.rb
+++ hello.rb
@@ -2,7 +2,15 @@ puts"hello"
 puts"Bonjour"
 puts"quel est votre nom?"
 name = gets
+
 puts "comment vas tu #{name}?"
-puts "salut #{name}"
-puts "salut #{name}"
-puts "salut #{name}"
+
+puts "salut #{name}"*3
+3.times do puts "salut #{name}"end
+
+puts "1".to_i
+
+
+
+
+
