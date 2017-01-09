09406ff23f9b0f9aee34bf531dfe94132006e608
hind <hind.lepert@cefimis.com>
2017-01-09 16:51:44 +0100

    methode dé 6 faces

--- dice.rb
+++ dice.rb
@@ -1,7 +1,12 @@
 #faire un programe qui affiche le résultat d'un tirage d'un dé à 6 faces
+# print result of a dice roll
+#roll = 1 + rand 6
 
 faces = [1, 2, 3, 4, 5, 6]
-roll = rand 6
+
+# roll = faces .shuffle
+
+roll = faces[rand 6]
 puts roll
 
 
