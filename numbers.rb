4d03d63609ae3218f80072695be70f9aeb3bd043
hind <hind.lepert@cefimis.com>
2017-01-09 15:06:24 +0100

    exo numbers1

---
+++ numbers.rb
@@ -0,0 +1,11 @@
+
+#exercices sur les nombres
+#calcul du nombre de secondes ds 1 année
+n=(12*30*24*60*60)
+puts "le nombre de secondes en 1 année est : #{n}"
+age=(40*n)
+puts "mon age en secondes est #{age}"
+#Time.now
+#Time.new(<année>,<mois>,<jour>,<heure>,<minutes>,<secondes>)
+puts Time.new("22/01/1977")
+
