314b7b5809231a79898cda63a511bad5f29c20f2
hind <hind.lepert@cefimis.com>
2017-01-09 16:08:49 +0100

    Add birt time

--- numbers.rb
+++ numbers.rb
@@ -7,5 +7,17 @@ age=(40*n)
 puts "mon age en secondes est #{age}"
 #Time.now
 #Time.new(<année>,<mois>,<jour>,<heure>,<minutes>,<secondes>)
-puts Time.new("22/01/1977")
+#puts Time.new("22/01/1977")
+#now = Time.now
+#birth_time = Time.new("22/01/1977")
+#age_in_sec = now.birth_time
+annee = Time.now.to_i/n
+puts annee
+anneorigine = Time.now.year - annee
+puts anneorigine
+puts Time.now.to_i%n
+
+
+
+
 
