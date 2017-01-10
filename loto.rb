141346add86c9032b441a2ef256541525b67bd6b
hind <hind.lepert@cefimis.com>
2017-01-10 15:08:06 +0100

    vendredi 13

--- loto.rb
+++ loto.rb
@@ -1,3 +1,22 @@
 #afficher un tirage de 5 nombres entre 1 et 45
-tirage = Array.new(49){|n|n+1}.shuffle.take(5).sort
-print "Tirage #{tirage}"	#affichage du tirage
+#tirage = Array.new(49){|n|n+1}.shuffle.take(5).sort
+#print "Tirage #{tirage}"	#affichage du tirage
+ 
+
+# affichage de la cagnote entre 100 et 500 euros
+
+# le vendredi 13, la cagnote est de 2 millions
+vendredi_13? = true
+if vendredi_13?
+    cagnote=2.000.000
+else 
+    cagnote = 100.000
+end
+
+puts "La cagnote du gain est de : #{}"
+
+
+available_balls = (1.45).to_a
+#suffle balls and teke 5
+picked_balls = available_balls.shuffle.take5
+
