77fea83b3eb327f4750412ab78a0dde399c0f219
hind <hind.lepert@cefimis.com>
2017-01-12 10:57:09 +0100

    correction

--- hash.rb
+++ hash.rb
@@ -1,4 +1,8 @@
-meta = { 14=>"ilyesse", 9=>"safya", 20=>"vincent"}
-place = [14]
-author = [20]
+meta = { 
+    14=>"Roubaix",
+    9=>"safya", 
+    20=>"vincent"
+}
+place = meta [14]
+author = meta [20]
 puts  "made with love in @#{place} by #{author}"
