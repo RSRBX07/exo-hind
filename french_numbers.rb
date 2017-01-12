8622a7050ee335f1d5f1e19f9bf6641c98b43119
hind <hind.lepert@cefimis.com>
2017-01-12 14:33:01 +0100

    modif1

--- french_numbers.rb
+++ french_numbers.rb
@@ -5,7 +5,30 @@ et qui retourne la chaine de carractere du chiffre ecrit en toutes lettres
 =end
 
 def french_numbers
-    fixnum = gets.to_i
-    fixnum =[zero:0, un:1, deux:2, troi:3, quatre:4, ..9]
+    case num
+    when 0
+        "zero"
+    when 1
+        "un"
+    when 2
+        "deux"
+    when 3
+        "trois"
+    when 4
+        "quatre"
+     when 5
+        "cinque"
+    when 6
+        "six"
+    when 7
+        "sept"
+    when 8
+        "huit"
+    when 9
+        "neuf"
+        else
+            nil
+    end
 
 end
+puts french_numbers
