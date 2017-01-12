6745e11fed3a115fa67d7f522c09395384991066
hind <hind.lepert@cefimis.com>
2017-01-12 14:40:32 +0100

    2eme correction

--- french_numbers.rb
+++ french_numbers.rb
@@ -4,7 +4,7 @@ qui prend une instance de fixnum en argument(accepte de 0 a 9)
 et qui retourne la chaine de carractere du chiffre ecrit en toutes lettres
 =end
 
-def french_numbers
+def french_numbers num
     case num
     when 0
         "zero"
@@ -17,7 +17,7 @@ def french_numbers
     when 4
         "quatre"
      when 5
-        "cinque"
+        "cinq"
     when 6
         "six"
     when 7
@@ -26,9 +26,9 @@ def french_numbers
         "huit"
     when 9
         "neuf"
-        else
-            nil
+    else
+        nil
     end
 
 end
-puts french_numbers
+puts french_numbers 5
