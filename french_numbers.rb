d46829da65e035809ede3de91c96ddddd953d780
hind <hind.lepert@cefimis.com>
2017-01-12 15:46:42 +0100

    j'arrive a vingt

--- french_numbers.rb
+++ french_numbers.rb
@@ -4,34 +4,7 @@ qui prend une instance de fixnum en argument(accepte de 0 a 9)
 et qui retourne la chaine de carractere du chiffre ecrit en toutes lettres
 =end
 
-def french_numbers num
-    case num
-    when 0
-        "zero"
-    when 1
-        "un"
-    when 2
-        "deux"
-    when 3
-        "trois"
-    when 4
-        "quatre"
-     when 5
-        "cinq"
-    when 6
-        "six"
-    when 7
-        "sept"
-    when 8
-        "huit"
-    when 9
-        "neuf"
-    else
-        nil
-    end
 
-end
-puts french_numbers 5
 
 
 def french_numbers index
@@ -41,3 +14,23 @@ end
 puts " saisir un chiffre entre 0 et 9"
 index = gets.chomp.to_i
 puts french_numbers index 
+
+
+def num_to_string number
+    if number.class == Fixnum
+        case number
+        when 0..16
+            ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf",
+             "dix","onze","douze","treize","quatorze","quinze","seize"][number]
+        when 17..19
+            "dix-" + num_to_string(number-10)
+        when 20..29
+            "vingt-" +num_to_string(number-19)
+        end
+   
+    end
+end
+
+(0..19).each {|n| puts num_to_string n}
+puts num_to_string 20
+
