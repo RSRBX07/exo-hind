dedf334e8bae32bd4ef160e7ba08daaae7710d20
hind <hind.lepert@cefimis.com>
2017-01-12 11:21:36 +0100

    departements_hasch

--- hash.rb
+++ hash.rb
@@ -1,3 +1,4 @@
+=begin 
 meta = { 
     14=>"Roubaix",
     9=>"safya", 
@@ -6,3 +7,13 @@ meta = {
 place = meta [14]
 author = meta [20]
 puts  "made with love in @#{place} by #{author}"
+=end
+departements = {
+    59 => 'Nord',
+    14 => 'Calvados',
+    11 => 'Aude',
+    75 => 'Paris',
+}
+puts "insérer le numéro d'un departement'"
+numero = gets.to_i
+puts "le #{numero} est le departement #{departements[numero]}"
