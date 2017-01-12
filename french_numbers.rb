cdc218d088f13c25e7cb6140793efb26ea5a5b3f
hind <hind.lepert@cefimis.com>
2017-01-12 14:59:02 +0100

    methode tableau

--- french_numbers.rb
+++ french_numbers.rb
@@ -4,8 +4,8 @@ qui prend une instance de fixnum en argument(accepte de 0 a 9)
 et qui retourne la chaine de carractere du chiffre ecrit en toutes lettres
 =end
 
-def french_numbers num
-    case num
+def french_numbers index
+    case index
     when 0
         "zero"
     when 1
@@ -32,3 +32,12 @@ def french_numbers num
 
 end
 puts french_numbers 5
+
+
+def french_numbers index
+      tableau = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"] 
+      return tableau [index]
+end 
+puts " saisir un chiffre entre 0 et 9"
+index = gets.chomp.to_i
+puts french_numbers index 
