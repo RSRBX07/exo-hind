83aba7620c94fb3a4cb30c634d57c3b81e21de46
hind <hind.lepert@cefimis.com>
2017-01-16 13:45:49 +0100

    correction pierre

--- test.rb
+++ test.rb
@@ -1,44 +1,28 @@
-require './dice.rb'
+# test de dice.rb
+# require './dice.rb'
 
-my_dice = Dice.new
+# my_dice = Dice.new
 
+# puts "lancer de de normal"
+# puts my_dice.roll
 
-
-puts "lancer de de normal"
-puts my_dice.roll 
-
-puts "lancer de de pip"
-puts my_dice.roll 4
-
-#faire un programe qui affiche le résultat d'un tirage d'un dé à 6 faces
-# print result of a dice roll
-#roll = 1 + rand 6
-
-faces = [1, 2, 3, 4, 5, 6]
-
-# roll = faces .shuffle
-
-roll = faces[rand 6]
-puts roll
-# ecrire une méthode roll qui va retourner la valeur d'un dé à 6 faces
+# puts "lancer de de pipe"
+# puts my_dice.roll 4
 
 # test de loto
 
 require './loto.rb'
-ma_grille = Grille.new
-le_tirage = Tirage.new
 
-puts "la grille est "
-puts ma_grille.demande_de_grille
-puts ma_grille.input_refinement raw_grid
+loto_du_samedi = Loto.new
+2.times {loto_du_samedi.validate_grid Loto.get_flash}
 
-puts "le tirage du jour est"
-puts le_tirage.make_draw 
-puts le_tirage.show_result sorted_grid, sorted_draw
+#.each { |grid| loto_du_samedi.check_grid grid }
 
-def grid_validation grid_validation
-
-
+loto_du_lundi = Loto.new
+loto_du_lundi.validate_grid Loto.get_flash
+#loto_du_lundi.check_grid(grid)
+if loto_du_lundi.has_winner?
+  puts "Someone win"
+else
+  puts "nobody win"
 end
-
-
