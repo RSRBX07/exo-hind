e3d5d3e510f4d0dc4ea49d017d087e2e25574b93
hind <hind.lepert@cefimis.com>
2017-01-16 15:49:06 +0100

    add methods include

--- loto.rb
+++ loto.rb
@@ -1,37 +1,43 @@
 require 'date'
 class Loto
 
-  def self.get_grid # on définit une méthode self.get_grid de classe : Loto, pour créer la grille = grid
+  def self.get_grid
     grid = []
     5.times do
-      input = gets.to_i
-      grid << input # << = push : pour ajouter dans la grille ce qui a été tapé clavier
+      begin  
+        input = gets.to_i
+        puts("nombre déjà utilisé !!!") if grid.include? input
+      end while grid.include? input # je reboucle si input existe déjà dans grid
+      grid << input
     end
     grid
-   puts "votre grille est : #{grid}" 
   end
 
-  def self.get_flash # flash : methode qui nous donne un tableau de 5 premiers chiffres allant de 1 à 45
+  def self.get_flash
     (1..45).to_a.shuffle.take 5
   end
 
-  def has_winner? # on définit une méthode d'instance de classe 
+  def has_winner?
     #comprer tous les bulletins valides avec la grille gagnante
     sorted_draw = draw.sort
     @saved_grids.each do |grid|
       sorted_grid = grid.sort
-      ifreturne true if sorted_grid== sorted_draw
-      end
-      #sorted_grid == sorted_draw
+      return true if sorted_grid == sorted_draw
     end
-    returne false
+    return false
   end
 
   # enregistre une grille
   # pour le loto courant
   def validate_grid grid
-    @saved_grids ||= []
-    @saved_grids.push grid
+    # @saved_grids ||= []
+    @saved_grids = @saved_grids || []
+    if  ! @picked_balls 
+        @saved_grids.push grid
+    else
+        puts "tirage déja fait, reviens demain !!!"
+    end
+    
   end 
   # demander une grille de jeu
 
@@ -39,17 +45,17 @@ class Loto
   # entre 100 et 500.000 Euros
   # le vendredi 13, la cagnote est de 2 millions
   def vendredi_13?
-    Date.today.day == 13 and Date.today.friday?
+    Date.today.day == 13 && Date.today.friday?
   end
 
   
 
   def prize
     cagnote = if vendredi_13?
-      2_000_000
-    else
-      100_000
-    end
+        2_000_000
+      else
+        100_000
+      end
     puts "Le montant de la cagnote du jour est de #{cagnote}"
     cagnote
   end
@@ -57,7 +63,8 @@ class Loto
   def draw
     available_balls = (1..45).to_a
     # shuffle balls and take 5
-    @picked_balls ||= available_balls.shuffle.take(5)
+    # @picked_balls ||= available_balls.shuffle.take(5)
+    @picked_balls = @picked_balls || (available_balls.shuffle.take 5)
 
     puts "Le tirage du jour est : #{@picked_balls.sort}" 
     @picked_balls
@@ -65,8 +72,7 @@ class Loto
 
   def check_grid grid
     # afficher si gagne ou perdu
-    if grid.sort ==
-       draw.sort
+    if grid.sort == draw.sort
       puts "You win !"
     else
       puts "You loose !"
