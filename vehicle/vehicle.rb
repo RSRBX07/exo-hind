1a7b8c2a341c9b4a2c677abc7e56b008b11f3e66
hind <hind.lepert@cefimis.com>
2017-01-18 17:44:05 +0100

    add plane and firetruck with variable static @@counter

--- vehicle/vehicle.rb
+++ vehicle/vehicle.rb
@@ -1,25 +1,40 @@
 class Vehicle 
 
+  @@counter = 0
+
   attr_reader :position
   attr_reader  :out_of_order
 
-  def self.new
-puts "I'm starting creation of a new vehicle"
-super
-puts "I finish creation of a new vehicle "
-  end 
-
+  # def self.new
+  #     puts "I'm starting creation of a new vehicle"
+  #     super
+  #     puts "I finish creation of a new vehicle "
+  # end 
+  
+  # return the number of existing objects (in memory)
+  # open irb
+  # Vehicle.count
+  #  #=>0
+  # Vehicle.new
+  # Vehicle.count
+  #  #=>1
+  
   def initialize 
-  puts "I'm starting initilization my instance of a new vehicle"
-    @out_of_order = false
-    puts "I finish initilization my instance of a new vehicle""
+    
+      puts "I'm starting initilization my instance of a new vehicle"
+      @out_of_order = false
+      puts "I finish initilization my instance of a new vehicle"
+      
+      @@counter += 1
+      
   end 
 
   def move(destination) 
-    @position = destination
-    puts " I moving "
+      @position = destination
+      puts " I moving "
   end
-
-
-
+  
+  def self.counter
+      @@counter
+  end
 end
