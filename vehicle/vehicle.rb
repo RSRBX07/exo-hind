27be19585814fe40dbb70ec6cd37e7656b3f2a8e
hind <hind.lepert@cefimis.com>
2017-01-18 16:40:06 +0100

    hello inheritance

--- vehicle/vehicle.rb
+++ vehicle/vehicle.rb
@@ -2,10 +2,19 @@ class Vehicle
 
   attr_reader :position
   attr_reader  :out_of_order
-  def initialize 
-    @position = "Roubaix"
-    @out_of_order = false
+
+  def self.new
+puts "I'm starting creation of a new vehicle"
+super
+puts "I finish creation of a new vehicle "
   end 
+
+  def initialize 
+  puts "I'm starting initilization my instance of a new vehicle"
+    @out_of_order = false
+    puts "I finish initilization my instance of a new vehicle""
+  end 
+
   def move(destination) 
     @position = destination
     puts " I moving "
