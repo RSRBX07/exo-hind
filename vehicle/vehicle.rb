6642a135c872846efcc4d0b872d7e9afe57c9fc6
hind <hind.lepert@cefimis.com>
2017-01-19 13:48:24 +0100

    add file counter.txt

--- vehicle/vehicle.rb
+++ vehicle/vehicle.rb
@@ -1,25 +1,60 @@
-class Vehicle 
+# require 'file'
 
+
+class Counter
+ def add_one
+   new_val = value + 1
+    File.open "./tmp/couneter.txt", "w" do |counter_file|
+      counter_file.write new_val
+  end
+
+  def value
+    # counter_file = File.new "./tmp/counter.txt", "r" # je récupére le fichier counter.txt en lecture seule
+    # counter_file
+    File.open "./tmp/couneter.txt", "r" do |counter_file|
+      counter_file.each_line{|line|return line.to_i}
+  end
+end
+
+
+
+class Vehicle 
   attr_reader :position
   attr_reader  :out_of_order
 
-  def self.new
-puts "I'm starting creation of a new vehicle"
-super
-puts "I finish creation of a new vehicle "
-  end 
+  #@counter = 0
 
+  # def self.new
+  #     puts "I'm starting creation of a new vehicle"
+  #     super
+  #     puts "I finish creation of a new vehicle "
+  # end 
+  
+  # return the number of existing objects (in memory)
+  # open irb
+  # Vehicle.count
+  #  #=>0
+  # Vehicle.new
+  # Vehicle.count
+  #  #=>1
+  
   def initialize 
-  puts "I'm starting initilization my instance of a new vehicle"
-    @out_of_order = false
-    puts "I finish initilization my instance of a new vehicle""
-  end 
+    
+      puts "I'm starting initilization my instance of a new vehicle"
+      @out_of_order = false
+      puts "I finish initilization my instance of a new vehicle"
+      Counter.new.add_one
+      
+  end
+  def self.count 
+    Counter.new.value
 
   def move(destination) 
-    @position = destination
-    puts " I moving "
+      @position = destination
+      puts " I moving "
   end
-
-
-
+  
+  def self.counter
+      @counter
+  end
 end
